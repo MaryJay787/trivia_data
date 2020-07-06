90a74da79ce293f04617ec71dd4fe38fbcc747de
MaryJay787 <josephmaryam.mj@gmail.com>
2020-07-06 01:25:14 +0000

    seeded some data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,5 @@
+Category.destroy_all
+Question.destroy_all
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
@@ -5,3 +7,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Category.create(title: 'Pillars of Islam')
+Category.create(title: 'Prophets')
+Category.create(title: 'Women in Islam')
+Category.create(title: 'Quran')
+Category.create(title: 'Hadith')
+
+Question.create(category_id: Category.first.id, 
+answer: 'Give Zakat', 
+ask: 'According to the Five Pillars of Islam, every Muslim must:', 
+option_a: 'Pray Morning and Night', 
+option_b: 'Read Daily', 
+option_c: 'Give Zakat', 
+option_d: 'Greet the Elderly')
